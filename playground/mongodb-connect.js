e30c7e944eac1dbb2d1358c89e3875700167cc20
nikhil surani <nikhil.surani@digi-corp.com>
2018-04-11 12:12:50 +0530

    create MongoDB connect using MOngoDB client and insert and fetch record from db

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,39 @@
+// const MongoClient = require("mongodb").MongoClient;
+const {MongoClient, ObjectID} = require("mongodb");
+var obj = new ObjectID;
+console.log(obj);
+// console.log(MongoClient, undefined, 2);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err){
+        return console.log("Unable to connect MongoDB server");
+    }
+    console.log("Connect to MongoDB server.");
+
+    // db.collection('Todos').insertOne({
+    //     text : "Nikhil",
+    //     completed : 'Yes'
+    // }, (err, result)=>{
+    //     if(err){
+    //         return console.log("Inable to insert", err);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+
+    // });
+
+    // db.collection('Users').insertOne({
+    //     _id: 123,
+    //     name : "Nikhil",
+    //     age : 23,
+    //     location : "ahmedabad"
+    // }, (err, result) => {
+    //     if(err){
+    //         return console.log(err);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+
+    // });
+
+    db.close();
+
+});
