332ead9b34fa349545cba4bf5b34826bba039cd1
nikhil surani <nikhil.surani@digi-corp.com>
2018-04-13 12:31:27 +0530

    ADD DELETE /todo/:id route

--- server/server.js
+++ server/server.js
@@ -47,8 +47,24 @@ app.get('/todo/:id', (req, res) => {
     }) 
 });
 
+// REMOVE by input id
+app.delete('/todo/:id', (req, res) =>{
+    let id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });   
+});
 
-// PORT
+
+// PORT Status
 app.listen(3000, () => {
     console.log("Started on port 3000");
 });
